3a3d1fd140cccf65854c52e38504aae6bda9d5ed
zhuxiuwu <1453855454@qq.com>
2019-12-04 13:05:12 +0800

    初始化

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	_ "myAppApi/routers"
+
+	"github.com/astaxie/beego"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
